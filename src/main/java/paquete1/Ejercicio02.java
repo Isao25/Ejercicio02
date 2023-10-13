fb2764ad2a63ea0171f64729b07fd2778e71c697
Isao25 <shamir.mantilla@unmsm.edu.pe>
2023-10-13 01:26:29 -0500

    hola bb

---
+++ src/main/java/paquete1/Ejercicio02.java
@@ -0,0 +1,18 @@
+/*
+Ejercicio 2: Construir un programa que
+permita dirigir el movimiento de un objeto
+dentro de un tablero y actualice su posición
+dentro del mismo. Los movimientos posibles
+son ARRIBA, ABAJO, IZQUIERDA y DERECHA
+Tras cada movimiento el programa mestrar
+la nueva dirección elegida y las coordèriadas
+de situación del objeto dentro del tablero.
+*/
+package paquete1;
+
+public class Ejercicio02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
